8aef0e363103a210b660b6b99b434c42b304b246
Suzun0suke <76026243+Suzun0suke@users.noreply.github.com>
2020-12-30 20:54:13 +0900

    ファイル過不足

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,27 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  with_options presence: true do
+    validates :nickname
+    validates :last_name
+    validates :first_name
+    validates :last_name_f
+    validates :first_name_f
+    validates :birthday
+  end
+  # validates :email, uniqueness: { case_sensitive: true }
+  # 全角（漢字・ひらがな・カタカナ）
+  validates :encrypted_password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+  with_options format: {with: /\A[ぁ-んァ-ン一-龥]/} do
+    validates :last_name
+    validates :first_name
+  end
+  # 全角（カタカナ）
+  with_options format: {with: /\A[ァ-ヶー－]+\z/} do
+    validates :last_name_f
+    validates :first_name_f
+  end
+
+
 end
