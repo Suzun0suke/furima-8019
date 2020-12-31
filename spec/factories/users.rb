0454c2f3ede415e5e9c8b96d852412889dbc714a
Suzun0suke <76026243+Suzun0suke@users.noreply.github.com>
2020-12-31 13:03:59 +0900

    モデルの単体テストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {"abc1111"}
     password_confirmation {password}
     last_name             {"苗字"}
     first_name            {"名前"}
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,50 +12,89 @@ RSpec.describe User, type: :model do
     end
     context "新規登録が情報が埋まってなくて登録できない" do
       it "ニックネームが空" do
+        @user.nickname = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank") 
       end
       it "メールアドレスが空" do
+        @user.email = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank") 
       end
       it "パスワードが空" do
+        @user.password = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank") 
       end
       it "苗字が空" do
+        @user.last_name = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name can't be blank") 
       end
       it "名前が空" do
+        @user.first_name = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name can't be blank") 
       end
       it "ミョウジが空" do
+        @user.last_name_f = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name f can't be blank") 
       end
       it "ナマエが空" do
+        @user.first_name_f = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name f can't be blank") 
       end
       it "誕生日が空" do
+        @user.birthday = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birthday can't be blank") 
       end
     end
     context "別途設定したvalidatesで登録できない場合" do
       it "同じemailでの登録" do
-        
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
       end
       it "emailに@がない" do
-        
+        @user.email = "test.com"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
       end
       it "passwordが6文字以下" do
-        
+        @user.password = "abc1"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
       end
       it "passwordに半角数字が混ざっていない" do
-        
+        @user.password = "123456"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
       end
       it "苗字が全角でない" do
-        
+        @user.last_name = "abc"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name is invalid")
       end
       it "名前が全角でない" do
-        
+        @user.first_name = "abc"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name is invalid")
       end
       it "ミョウジが全角カナでない" do
-        
+        @user.last_name_f = "ﾐｮｳｼﾞ"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name f is invalid")
       end
       it "ナマエが全角カナでない" do
-        
+        @user.first_name_f = "ﾅﾏｴ"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name f is invalid")
       end
     end
-    
-    
-    
   end
 end
