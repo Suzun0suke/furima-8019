508d41d5b7b478604a8e177b104bf713bd1c14ef
Suzun0suke <76026243+Suzun0suke@users.noreply.github.com>
2021-01-04 14:20:15 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,10 +7,22 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
     end
+  end
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category_id, :state_id, :delivery_cost_id, :shipment_source_id, :shipping_day_id, :selling_price, :image).merge(user_id: current_user.id)
   end
 end
