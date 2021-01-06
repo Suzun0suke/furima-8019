5a48fcf2a850189233fa2826af630ff089d54d0e
Suzun0suke <76026243+Suzun0suke@users.noreply.github.com>
2021-01-06 21:32:16 +0900

    リファクタリング　v1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :item, only:[:show, :edit, :update, :move_to_index]
   before_action :move_to_index, :purchased_item ,only:[:edit]
   
-
   def index
     @items = Item.all.includes(:user).order("created_at DESC")
   end
@@ -21,17 +21,16 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    
   end
 
   def edit
-    @item = Item.find(params[:id])
+    
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to item_path(item.id)
+      redirect_to item_path(@item.id)
     else
       render :edit
     end
@@ -41,13 +40,16 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :explanation, :category_id, :state_id, :delivery_cost_id, :shipment_source_id, :shipping_day_id, :selling_price, :image).merge(user_id: current_user.id)
   end
+  def item
+    @item = Item.find(params[:id])
+  end
 
   def move_to_index
-    @item = Item.find(params[:id])
     if current_user.id != @item.user_id
     redirect_to root_path
     end
   end
+  
   def purchased_item
     unless @item.purchase.blank?
       redirect_to root_path
