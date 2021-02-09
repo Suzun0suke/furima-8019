8755c714646b46c7b16e752a87273e77f9021914
Suzun0suke <76026243+Suzun0suke@users.noreply.github.com>
2021-02-09 21:13:35 +0900

    Revert "カテゴリーページ"

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :users, only:[:show]
-  resources :categories, only:[:index, :show]
   resources :items do
     resources :purchases, only:[:index, :create]
   end
