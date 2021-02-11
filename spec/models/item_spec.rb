bfb8b936048c03bf6a615d5ca9b88f4d111396bc
Suzun0suke <76026243+Suzun0suke@users.noreply.github.com>
2021-02-11 16:54:50 +0900

    エラー文日本語実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -14,47 +14,47 @@ RSpec.describe Item, type: :model do
       it "画像がない" do
         @item.image = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Image can't be blank")
+        expect(@item.errors.full_messages).to include("画像を入力してください")
       end
       it "商品名がない" do
         @item.name = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Name can't be blank")
+        expect(@item.errors.full_messages).to include("商品名を入力してください")
       end
       it "説明がない" do
         @item.explanation = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Explanation can't be blank")
+        expect(@item.errors.full_messages).to include("商品の説明を入力してください")
       end
       it "カテゴリー情報がない" do
         @item.category_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category can't be blank")
+        expect(@item.errors.full_messages).to include("カテゴリーを入力してください")
       end
       it "商品状態がない" do
         @item.state_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("State can't be blank")
+        expect(@item.errors.full_messages).to include("商品の状態を入力してください")
       end
       it "配送料負担がない" do
         @item.delivery_cost_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Delivery cost can't be blank")
+        expect(@item.errors.full_messages).to include("発送料の負担を入力してください")
       end
       it "発送もとの情報がない" do
         @item.shipment_source_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipment source can't be blank")
+        expect(@item.errors.full_messages).to include("発送元の地域を入力してください")
       end
       it "発送までの日数がない" do
         @item.shipping_day_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping day can't be blank")
+        expect(@item.errors.full_messages).to include("発送までの日数を入力してください")
       end
       it "価格の情報がない" do
         @item.selling_price = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Selling price is not a number")
+        expect(@item.errors.full_messages).to include("販売価格は数値で入力してください")
       end
       
     end
@@ -62,27 +62,27 @@ RSpec.describe Item, type: :model do
       it "カテゴリー情報" do
         @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category must be other than 1")
+        expect(@item.errors.full_messages).to include("カテゴリーは --- 以外を選択してください")
       end
       it "商品状態" do
         @item.state_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("State must be other than 1")
+        expect(@item.errors.full_messages).to include("商品の状態は --- 以外を選択してください")
       end
       it "配送料負担" do
         @item.delivery_cost_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Delivery cost must be other than 1")
+        expect(@item.errors.full_messages).to include("発送料の負担は --- 以外を選択してください")
       end
       it "発送もと" do
         @item.shipment_source_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipment source must be other than 1")
+        expect(@item.errors.full_messages).to include("発送元の地域は --- 以外を選択してください")
       end
       it "発送までの日数がない" do
         @item.shipping_day_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping day must be other than 1")
+        expect(@item.errors.full_messages).to include("発送までの日数は --- 以外を選択してください")
       end
     end
     
@@ -90,27 +90,27 @@ RSpec.describe Item, type: :model do
       it "価格が300未満" do
         @item.selling_price = 100
         @item.valid?
-        expect(@item.errors.full_messages).to include("Selling price must be greater than or equal to 300")
+        expect(@item.errors.full_messages).to include("販売価格は300以上の値にしてください")
       end
       it "価格が10,000,000以上" do
         @item.selling_price = 10000000
         @item.valid?
-        expect(@item.errors.full_messages).to include("Selling price must be less than or equal to 9999999")
+        expect(@item.errors.full_messages).to include("販売価格は9999999以下の値にしてください")
       end
       it "価格が半角数字以外" do
         @item.selling_price = "５００"
         @item.valid?
-        expect(@item.errors.full_messages).to include("Selling price is not a number")
+        expect(@item.errors.full_messages).to include("販売価格は数値で入力してください")
       end
       it "価格が半角英数混合" do
         @item.selling_price = "abc100"
         @item.valid?
-        expect(@item.errors.full_messages).to include("Selling price is not a number")
+        expect(@item.errors.full_messages).to include("販売価格は数値で入力してください")
       end
       it "価格が半角英語" do
         @item.selling_price = "abc"
         @item.valid?
-        expect(@item.errors.full_messages).to include("Selling price is not a number")
+        expect(@item.errors.full_messages).to include("販売価格は数値で入力してください")
       end
     end
   end
